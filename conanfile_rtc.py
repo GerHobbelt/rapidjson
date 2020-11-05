3db2004924e47b033c3cf7879c31029399f23a58
Christian Venegas <cvenegas@esri.com>
2020-11-05 12:11:11 -0800

    build: Add a conanfile to package artifacts (#2)
    Issue-number: https://devtopia.esri.com/runtime/devops/issues/693

---
+++ conanfile_rtc.py
@@ -0,0 +1,22 @@
+from conans import ConanFile
+
+
+class RapidjsonConan(ConanFile):
+    name = "rapidjson"
+    version = "1.1.0"
+    url = "https://github.com/Esri/rapidjson/tree/runtimecore"
+    license = "https://github.com/Esri/rapidjson/blob/runtimecore/license.txt"
+    description = "A fast JSON parser/generator for C++ with both SAX/DOM style API."
+
+    # Use the OS default to get the right line endings
+    settings = "os"
+
+    def package(self):
+        base = self.source_folder + "/"
+        relative = "3rdparty/rapidjson/"
+
+        # headers
+        self.copy("*.h*", src=base + "include", dst=relative + "include")
+
+    def package_id(self):
+        self.info.header_only()
